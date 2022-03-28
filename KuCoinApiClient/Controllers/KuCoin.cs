92acdb2f2098d0822f142c95131cc8743f2af162
Vitalii Chyrka <viavital@ukr.net>
2022-03-28 22:01:04 +0300

    logger work properly

--- KuCoinApiClient/Controllers/KuCoin.cs
+++ KuCoinApiClient/Controllers/KuCoin.cs
@@ -19,41 +19,12 @@ namespace KuCoinApiClient.Controllers
 {
     public class KuCoin : Controller
     {
-
-        private async Task<string> GetToken()
-        {
-            HttpClient client = new HttpClient();
-            var content = new System.Net.Http.StringContent("");
-            HttpResponseMessage response = await client.PostAsync("https://api.kucoin.com/api/v1/bullet-public", content); // кидає запит
-            response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync(); // виводить результат запиту на екран
-            var TokenMessClass = JsonConvert.DeserializeObject<GetTokenMessage>(responseBody);
-            return TokenMessClass.data.token;
-        }
-
-        private async Task <string> GetMarketDataBySocket()
-        {
-            string WelcomeMessage = "null";
-            var tcs = new TaskCompletionSource();
-            string token = await GetToken();
-            using var socket = new WebSocket ("wss://ws-api.kucoin.com/endpoint?token=" + token);
-            socket.MessageReceived += (s, e) =>
-           {
-               WelcomeMessage = e.Message.ToString();
-               tcs.TrySetResult();
-           };
-            socket.Open();
-            await tcs.Task;
-            return WelcomeMessage;
-           // ("{\"id\":1545910660740, \"type\":\"subscribe\",\"topic\": \"/spotMarket/level2Depth5:BTC-USDT\", \"response\": true }")), WebSocketMessageType.Text, false, System.Threading.CancellationToken.None);
-                    
-        }
     public async Task<IActionResult> Market()
         { 
-            var message =  await GetMarketDataBySocket();   
+            var message = "Look on Console!!!";   
             ViewBag.MarketData = message;            
             return View();
         }
     }        
-}
+ }
 
--- KuCoinApiClient/Startup.cs
+++ KuCoinApiClient/Startup.cs
@@ -1,13 +1,16 @@
+using KuCoinApiClient.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebSocket4Net;
 
 namespace KuCoinApiClient
 {
@@ -22,13 +25,12 @@ namespace KuCoinApiClient
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSignalRCore();
+        {           
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -49,6 +51,18 @@ namespace KuCoinApiClient
 
             app.Use(async (context, next) =>
             {
+                string MessageFromSocket = "null";
+                var tcs = new TaskCompletionSource();
+                GetTokenMessageService getTokenMessageServ = new GetTokenMessageService();
+                var token = await getTokenMessageServ.GetToken();
+                var socket = new WebSocket("wss://ws-api.kucoin.com/endpoint?token=" + token);
+                socket.MessageReceived += (s, e) =>
+                {
+                    MessageFromSocket = e.Message.ToString();
+                    logger.LogInformation(MessageFromSocket);
+                };
+                socket.Opened += (s, e) => { socket.Send("{\"id\":1545910660740, \"type\":\"subscribe\",\"topic\": \"/spotMarket/level2Depth5:BTC-USDT\", \"response\": true }"); };
+                socket.Open();
                 await next.Invoke();
             });
             app.UseEndpoints(endpoints =>
